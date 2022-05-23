9980cf5b9bca0a7be8862f9d3f932f3aae0f6c04
zhongzero <1144265105@qq.com>
2022-05-23 13:30:08 +0800

    zhongzero 2022.5.23 13:30

---
+++ cmake/CMakeFiles/code.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/e/TrainTicketSystem_2022/Order.cpp" "/mnt/e/TrainTicketSystem_2022/cmake/CMakeFiles/code.dir/Order.cpp.o"
+  "/mnt/e/TrainTicketSystem_2022/Trainticket.cpp" "/mnt/e/TrainTicketSystem_2022/cmake/CMakeFiles/code.dir/Trainticket.cpp.o"
+  "/mnt/e/TrainTicketSystem_2022/User.cpp" "/mnt/e/TrainTicketSystem_2022/cmake/CMakeFiles/code.dir/User.cpp.o"
+  "/mnt/e/TrainTicketSystem_2022/main.cpp" "/mnt/e/TrainTicketSystem_2022/cmake/CMakeFiles/code.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake/CMakeFiles/code.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/code.dir/Order.cpp.o"
+  "CMakeFiles/code.dir/Trainticket.cpp.o"
+  "CMakeFiles/code.dir/User.cpp.o"
+  "CMakeFiles/code.dir/main.cpp.o"
+  "code"
+  "code.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
